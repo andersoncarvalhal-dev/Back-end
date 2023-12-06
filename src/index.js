f54a80e406a9050bb3b941943df346db91034f8f
andersoncarvalhal-dev <andersonpimenta10@hotmail.com>
2023-12-06 15:07:46 -0300

    enviando5

--- src/index.js
+++ src/index.js
@@ -6,41 +6,44 @@ const cors = require("cors");
 
 const app = express();
 const prisma = new PrismaClient();
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 5000;
 
 //configurações para o servidor
 app.use(bodyParser.json());
 app.use(cors());
 
 //rota para criar um item
+app.post("/item", async (req, res) => {
+  const dados = req.body;
+  await prisma.item.create({
+    data: {
+      nome: dados.nome,
+    },
+  });
+  return res.sendStatus(201);
+});
+
+//rota para listar todos os usuários
 app.get("/users", async (req, res) => {
   const users = await prisma.user.findMany();
   if (users.length > 0) return res.status(200).send(users);
   return res.send("No users found");
 });
 
-app.post("/user", async (req, res) => {
-  const data = req.body;
-  await prisma.user.create({
-    data: {
-      name: data.nome,
-    },
-  });
-  return res.sendStatus(201);
-});
 
 //rota para buscar um usuário pelo nome
 app.get("/user/:name", async (req, res) => {
-  const nome = req.params.name;
+  const name = req.params.name;
   const user = await prisma.user.findMany({
     where: {
-      name: nome,
+      name: name,
     },
   });
   if (user.length > 0) return res.status(200).send(user);
   return res.send("No user found");
 });
 
+//finalizado
 
 // Inicie o servidor na porta especificada
 app.listen(port, () => {
